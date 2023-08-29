737d8b0ac301d8d1ce0262147f789bcfed16af8f
Hasibur Rahman <139708138+HasiburRahman02@users.noreply.github.com>
2023-08-29 08:16:20 +0600

    Button with Listener2 in App Development.

---
+++ app/src/main/java/com/example/buttonwithlistener2/MainActivity.java
@@ -0,0 +1,48 @@
+/*  Button with Listener2 in App Development.
+
+    Date:29 August,2023
+    Author:Hasibur Rahman
+
+ */
+
+package com.example.buttonwithlistener2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+    private Button loginButton,logoutButton;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginButton = (Button) findViewById(R.id.loginButtonId);
+        logoutButton = (Button) findViewById(R.id.logoutButtonId);
+        textView = (TextView) findViewById(R.id.textViewId);
+
+        loginButton.setOnClickListener(this);
+        logoutButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        if(view.getId()==R.id.loginButtonId)
+        {
+            textView.setText("Login Button is Clicked.");
+        }
+        if(view.getId()==R.id.logoutButtonId)
+        {
+            textView.setText("Logout Button is Clicked.");
+        }
+    }
+}
